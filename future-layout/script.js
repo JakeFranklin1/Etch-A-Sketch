5247c1d45c27b3fdcedd512dc1b5dfcfa4665ed3
Jake <Jakefranklin14@gmail.com>
2022-07-02 14:05:13 +0100

    fix title

--- future-layout/script.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.getElementsByTagName("button");
-
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", () => {
-    buttons[i].classList.toggle("btn-on");
-  });
-}
